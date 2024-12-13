6c17a38c8cf23b8618cfa736f71df5307656322b
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-12-13 12:45:42 -0300

    feat: add function to write the reports to a JSON file

--- internal/file/handler.go
+++ internal/file/handler.go
@@ -2,10 +2,12 @@ package file
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/vhrboliveira/quake-log-parser-test/internal/logparser"
 )
 
 func ReadFile(path string, lines chan<- string) {
@@ -28,4 +30,30 @@ func ReadFile(path string, lines chan<- string) {
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("error reading file: %v", err)
 	}
+}
+
+func WriteFile(path string, gameReport <-chan logparser.GameReport, done chan<- bool) {
+	fileName := path + ".json"
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	for event := range gameReport {
+		jsonData, err := json.MarshalIndent(event, "", "  ")
+		if err != nil {
+			fmt.Printf("Error marshaling JSON: %v\n", err)
+			return
+		}
+
+		_, err = file.Write(jsonData)
+		if err != nil {
+			fmt.Printf("Error writing to file: %v\n", err)
+			return
+		}
+	}
+
+	done <- true
 }
