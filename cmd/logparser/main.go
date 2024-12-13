16b2685beb93708951e78bcc71b07af13721b3e7
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-12-13 12:55:03 -0300

    feat: update the main script to read the file, process the log, and generate the reports

--- cmd/logparser/main.go
+++ cmd/logparser/main.go
@@ -1,8 +1,26 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/vhrboliveira/quake-log-parser-test/internal/file"
+	"github.com/vhrboliveira/quake-log-parser-test/internal/logparser"
+)
+
 func main() {
 	filePath := os.Getenv("LOG_FILE")
 	if filePath == "" {
 		log.Fatal("environment variable LOG_FILE is not set. Please set the LOG_FILE environment variable to the path of the quake log file. Ex: \"assets/quake.log\"")
 	}
+
+	lines := make(chan string)
+	gameReport := make(chan logparser.GameReport)
+	done := make(chan bool)
+
+	go file.ReadFile(filePath, lines)
+	go logparser.ParseLines(lines, gameReport)
+	go file.WriteFile(filePath, gameReport, done)
+
+	<-done
 }
