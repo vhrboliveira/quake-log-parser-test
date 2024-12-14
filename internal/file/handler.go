67d6f543324b522fe981aacc81a225ee1c74c02c
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-12-13 21:20:10 -0300

    fix: handling channel erros on file handler

--- internal/file/handler.go
+++ internal/file/handler.go
@@ -4,18 +4,19 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/vhrboliveira/quake-log-parser-test/internal/logparser"
 )
 
-func ReadFile(path string, lines chan<- string) {
+func ReadFile(path string, lines chan<- string, errChan chan<- error) {
 	fmt.Println("opening quake log file:", path)
 
 	file, err := os.Open(path)
 	if err != nil {
-		log.Fatalf("failed to open quake log file: %v", err)
+		errChan <- fmt.Errorf("failed to open quake log file: %w", err)
+		close(lines)
+		return
 	}
 	defer file.Close()
 
@@ -25,18 +26,19 @@ func ReadFile(path string, lines chan<- string) {
 		lines <- scanner.Text()
 	}
 
-	close(lines)
-
 	if err := scanner.Err(); err != nil {
-		log.Fatalf("error reading file: %v", err)
+		errChan <- fmt.Errorf("error reading file: %w", err)
 	}
+
+	close(lines)
 }
 
-func WriteFile(path string, gameReport <-chan logparser.GameReport, done chan<- bool) {
+func WriteFile(path string, gameReport <-chan logparser.GameReport, done chan<- bool, errChan chan<- error) {
 	fileName := path + ".json"
 	file, err := os.Create(fileName)
 	if err != nil {
-		fmt.Printf("Error creating file: %v\n", err)
+		errChan <- fmt.Errorf("error creating file: %w", err)
+		done <- false
 		return
 	}
 	defer file.Close()
@@ -44,13 +46,15 @@ func WriteFile(path string, gameReport <-chan logparser.GameReport, done chan<- 
 	for event := range gameReport {
 		jsonData, err := json.MarshalIndent(event, "", "  ")
 		if err != nil {
-			fmt.Printf("Error marshaling JSON: %v\n", err)
+			errChan <- fmt.Errorf("error marshaling JSON: %w", err)
+			done <- false
 			return
 		}
 
 		_, err = file.Write(jsonData)
 		if err != nil {
-			fmt.Printf("Error writing to file: %v\n", err)
+			errChan <- fmt.Errorf("error writing to file: %w", err)
+			done <- false
 			return
 		}
 	}
